686ffc825845733f015daaf11b3951062c162160
Abr009 <bradithya945@gmail.com>
2024-09-08 18:37:51 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Stopwatch Application
+
+A simple and responsive stopwatch web app built using HTML, CSS, and JavaScript. It includes Start, Pause, and Reset functionalities.
+
+## Features
+
+- **Start**: Begin the stopwatch.
+- **Pause**: Pause the stopwatch.
+- **Reset**: Reset the time to `00:00:00`.
+
+## How to Use
+
+1. Visit the [Stopwatch Application on Netlify](https://your-netlify-link).
+2. Use the buttons to control the stopwatch.
+
---
+++ script.js
@@ -0,0 +1,55 @@
+const min = document.getElementById('min');
+const sec = document.getElementById('sec');
+const milSec = document.getElementById('milSec');
+
+var tmin = 0;
+var tsec = 0;
+var tmilSec = 0;
+var pause = false
+var stopWatch
+
+function manipulateTime() {
+  tmin
+  tsec
+  tmilSec
+  pause
+
+  if (tmilSec == 100) {
+    tmilSec = 0;
+    tsec++;
+  }
+  if (tsec == 60) {
+    tsec = 0;
+    tmin++;
+  }
+
+  min.innerText = tmin
+  sec.innerText = tsec
+  milSec.innerText = tmilSec
+
+  if (!pause)
+    tmilSec++
+}
+
+function TStart() {
+  clearInterval(stopWatch)
+  pause = false
+  stopWatch = setInterval(manipulateTime, 10)
+}
+
+function TPause() {
+  pause = true
+}
+
+function TReset() {
+  clearInterval(stopWatch)
+
+  min.innerText = 0
+  sec.innerText = 0
+  milSec.innerText = 0
+
+  tmin = 0
+  tsec = 0
+  tmilSec = 0
+  pause = false
+}
